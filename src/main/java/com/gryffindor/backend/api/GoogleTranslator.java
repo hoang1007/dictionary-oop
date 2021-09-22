fc015ddc7ebb097e8aaedb82a9094475aeb5875f
hoang1007 <hoangvuhuy1007@gmail.com>
2021-09-22 09:56:16 +0700

    add google translate api

---
+++ src/main/java/com/gryffindor/backend/api/GoogleTranslator.java
@@ -0,0 +1,35 @@
+package com.gryffindor.backend.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import com.gryffindor.App;
+import com.gryffindor.Language;
+
+public class GoogleTranslator {
+  public static String translate(String word_target, Language from, Language to) throws IOException {
+    String urlString = App.INSTANCE.config.getGoogleAPIUrl() + 
+                    "?q=" + URLEncoder.encode(word_target, "UTF-8") +
+                    "&target=" + to.toShortString() + "&from=" + from.toShortString();
+
+    URL url = new URL(urlString);
+
+    StringBuffer response = new StringBuffer();
+
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+    for (String inputLine; (inputLine = reader.readLine()) != null; ) {
+      response.append(inputLine);
+    }
+
+    reader.close();
+    return response.toString();
+  }
+}
