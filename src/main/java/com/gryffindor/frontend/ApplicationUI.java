b473ba31087e79b2f10df7a3180883f03840d8d6
hoang1007 <hoangvuhuy1007@gmail.com>
2021-09-23 15:43:20 +0700

    nơi để khởi tạo đồ họa

---
+++ src/main/java/com/gryffindor/frontend/ApplicationUI.java
@@ -0,0 +1,29 @@
+package com.gryffindor.frontend;
+
+import com.gryffindor.frontend.scenes.mainscene.MainScene;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class ApplicationUI extends Application {
+  final String title;
+  final int width;
+  final int height;
+
+  /** Khởi tạo UI. */
+  public ApplicationUI() {
+    this.title = "Gryffindor Dictionary";
+    width = 500;
+    height = 800;
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setWidth(width);
+    primaryStage.setHeight(height);
+    
+    primaryStage.setScene(new MainScene().getMainScene());
+    primaryStage.show();
+  }
+}
