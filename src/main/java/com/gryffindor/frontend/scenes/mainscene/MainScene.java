b39863d16aa71228c8b350403e7d9a4d1ac9caab
hoang1007 <hoangvuhuy1007@gmail.com>
2021-09-24 16:51:17 +0700

    chỉnh lại layout

--- src/main/java/com/gryffindor/frontend/scenes/mainscene/MainScene.java
+++ src/main/java/com/gryffindor/frontend/scenes/mainscene/MainScene.java
@@ -1,7 +1,10 @@
 package com.gryffindor.frontend.scenes.mainscene;
 
 import com.gryffindor.DictionaryApplication;
+import com.gryffindor.frontend.scenes.mainscene.entities.ExplainField;
+import com.gryffindor.frontend.scenes.mainscene.entities.NavigationBarField;
 import com.gryffindor.frontend.scenes.mainscene.entities.SearchField;
+import com.gryffindor.frontend.scenes.mainscene.entities.TranslateField;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
@@ -11,13 +14,31 @@ public class MainScene {
   private final Scene mainScene;
   private final VBox rootPane;
 
+  private final NavigationBarField navigationBarField;
+  private final SearchField searchField;
+  private final TranslateField translateField;
+  private final ExplainField explainField;
+
   /** Khởi tạo scene chính. */
   public MainScene() {
     rootPane = new VBox();
+    rootPane.getStyleClass().add("root-pane");
+    rootPane.setSpacing(5); // đặt khoảng cách giữa các child pane
+
+    navigationBarField = new NavigationBarField();
+    searchField = new SearchField();
+    translateField = new TranslateField();
+    explainField = new ExplainField();
+
+    rootPane.getChildren().addAll(
+        navigationBarField.getPane(),
+        searchField.getPane(), 
+        translateField.getPane(), 
+        explainField.getPane()
+    );
+
     mainScene = new Scene(rootPane);
     mainScene.getStylesheets().add(DictionaryApplication.INSTANCE.config.getStyle());
-  
-    rootPane.getChildren().add(new SearchField().getPane());
   }
 
   public Scene getMainScene() {
