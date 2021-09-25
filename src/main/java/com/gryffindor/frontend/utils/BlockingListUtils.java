1abff7760309381976a554b67f655a46e4cf3894
hoang1007 <hoangvuhuy1007@gmail.com>
2021-09-25 17:16:45 +0700

    thêm công cụ thao tác trên danh sách giới hạn phần tử

---
+++ src/main/java/com/gryffindor/frontend/utils/BlockingListUtils.java
@@ -0,0 +1,67 @@
+package com.gryffindor.frontend.utils;
+
+import java.util.Arrays;
+
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+
+/** Công cụ thao tác trên ListView khi muốn giới hạn số lượng. */
+public class BlockingListUtils<T> {
+  private final int limit;
+  private ObservableList<T> list;
+
+  public BlockingListUtils(int limit, ListView<T> listView) {
+    this.limit = limit;
+    this.list = listView.getItems();
+  }
+
+  /**
+   * Thêm phần tử vào cuối danh sách nếu danh sách chứa ít hơn số lượng đã đặt.
+   * @param element phần tử muốn thêm vào danh sách
+   */
+  public void add(T element) {
+    if (list.size() < limit) {
+      list.add(element);
+    }
+  }
+
+  /**
+   * Thêm các phần tử vào cuối danh sách với giới hạn đặt trước.
+   * @param elements các phần tử muốn thêm
+   */
+  @SuppressWarnings("unchecked")
+  public void addAll(T... elements) {
+    for (T element : elements) {
+      add(element);
+    }
+  }
+
+  /**
+   * Thay thế phần tử ở vị trí cho trước.
+   * @param index vị trí muốn thay thế
+   * @param element giá trị muốn thay thế
+   */
+  public void set(int index, T element) {
+    if (index < limit) {
+      list.set(index, element);
+    }
+  }
+
+  /**
+   * Xóa hết các phần tử trong danh sách
+   * và thay thế bằng các phần tử mới.
+   * @param elements các phần tử muốn thay thế
+   */
+  @SuppressWarnings("unchecked")
+  public void setAll(T... elements) {
+    if (elements.length > limit) {
+      elements = Arrays.copyOf(elements, limit);
+    }
+
+    list.setAll(elements);
+  }
+
+  public int getLimit() {
+    return limit;
+  }
+}
