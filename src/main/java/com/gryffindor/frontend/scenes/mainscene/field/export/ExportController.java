3b37ca1ab3eb7fa6da7d2549fcae16fe7025d172
hoang1007 <hoangvuhuy1007@gmail.com>
2021-09-28 15:51:50 +0700

    vùng chứa tiện ích xuất file

---
+++ src/main/java/com/gryffindor/frontend/scenes/mainscene/field/export/ExportController.java
@@ -0,0 +1,7 @@
+package com.gryffindor.frontend.scenes.mainscene.field.export;
+
+import com.gryffindor.frontend.scenes.mainscene.field.IController;
+
+public class ExportController implements IController {
+  
+}
---
+++ src/main/java/com/gryffindor/frontend/scenes/mainscene/field/export/ExportField.java
@@ -0,0 +1,51 @@
+package com.gryffindor.frontend.scenes.mainscene.field.export;
+
+import com.gryffindor.DictionaryApplication;
+import com.gryffindor.frontend.scenes.mainscene.field.IController;
+import com.gryffindor.frontend.scenes.mainscene.field.IField;
+import com.gryffindor.frontend.utils.ImageUtils;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class ExportField implements IField {
+  private final Pane pane;
+
+  private Button exportButton;
+
+  /** Khởi tạo vùng export. */
+  public ExportField() {
+    pane = new Pane();
+
+    initExportButton();
+
+    pane.getChildren().add(exportButton);
+  }
+
+  void initExportButton() {
+    exportButton = new Button("Export words to file");
+    exportButton.getStyleClass().add("tool-button");
+
+    exportButton.maxWidthProperty().set(Integer.MAX_VALUE);
+
+    ImageView toolsImageView = ImageUtils.getFitSquareImage(
+        DictionaryApplication.INSTANCE.config.getImagesPath() + "/export.png", 
+        50);
+    exportButton.setGraphic(toolsImageView);
+  }
+
+  @Override
+  public Pane getPane() {
+    return pane;
+  }
+
+  public Button getExportButton() {
+    return exportButton;
+  }
+
+  @Override
+  public IController getController() {
+    return null;
+  }
+}
