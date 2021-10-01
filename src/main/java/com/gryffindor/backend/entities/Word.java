4199f4041371a971f4b69a2082cdce888b24ec1b
johnhoangsoftware <johnhoangsoftware@gmail.com>
2021-10-01 23:36:47 +0700

    sửa lại class Word- thêm loại từ và vidu

--- src/main/java/com/gryffindor/backend/entities/Word.java
+++ src/main/java/com/gryffindor/backend/entities/Word.java
@@ -1,32 +1,59 @@
 package com.gryffindor.backend.entities;
 
 public class Word {
-  private String wordTarget;
-  private String wordExplain;
+    private String wordTarget;
+    private String wordExplain;
+    private String wordSpelling = null; // phiên âm.
+    private String wordType = null; // loai tu
 
-  public Word(String wordTarget, String wordExplain) {
-    this.wordTarget = wordTarget;
-    this.wordExplain = wordExplain;
-  }
+    public Word(String wordTarget, String wordExplain) {
+        this.wordTarget = wordTarget;
+        this.wordExplain = wordExplain;
+        this.wordSpelling = null;
+        this.wordType = null;
+    }
 
-  public void setWordTarget(String wordTarget) {
-    this.wordTarget = wordTarget;
-  }
+    /** Constructor have spelling */
+    public Word(String wordTarget, String wordSpelling, String wordExplain) {
+        this.wordTarget = wordTarget;
+        this.wordSpelling = wordSpelling;
+        this.wordExplain = wordExplain;
+        this.wordType = null;
+    }
 
-  public String getWordTarget() {
-    return this.wordTarget;
-  }
+    public Word(String wordTarget, String wordSpelling, String wordExplain, String wordType) {
+        this.wordTarget = wordTarget;
+        this.wordSpelling = wordSpelling;
+        this.wordExplain = wordExplain;
+        this.wordType = wordType;
+    }
 
-  public void setWordExplain(String wordExplain) {
-    this.wordExplain = wordExplain;
-  }
+    public void setWordTarget(String wordTarget) {
+        this.wordTarget = wordTarget;
+    }
 
-  public String getWordExplain() {
-    return this.wordExplain;
-  }
+    public String getWordTarget() {
+        return this.wordTarget;
+    }
 
-  @Override
-  public String toString() {
-    return wordTarget + "\n" + wordExplain;
-  }
+    public void setWordExplain(String wordExplain) {
+        this.wordExplain = wordExplain;
+    }
+
+    public String getWordExplain() {
+        return this.wordExplain;
+    }
+
+    public void setWordSpelling(String wordSpelling) { this.wordSpelling = wordSpelling; }
+
+    public String getWordSpelling() { return this.wordSpelling; }
+
+    public void setWordType(String wordType) { this.wordType = wordType; }
+
+    public String getWordType() { return this.wordType; }
+
+    @Override
+    public String toString() {
+        return wordTarget + "\n" + wordExplain;
+    }
 }
