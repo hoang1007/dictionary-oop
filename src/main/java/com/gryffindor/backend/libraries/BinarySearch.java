526b2d12aa9faa71b8a7c3646be4749ab5481984
johnhoangsoftware <johnhoangsoftware@gmail.com>
2021-10-04 22:49:36 +0700

    add method BinarySearch to the lib BinarySearch

--- src/main/java/com/gryffindor/backend/libraries/BinarySearch.java
+++ src/main/java/com/gryffindor/backend/libraries/BinarySearch.java
@@ -3,6 +3,33 @@ package com.gryffindor.backend.libraries;
 import com.gryffindor.backend.entities.Dictionary;
 import com.gryffindor.backend.entities.Word;
 
+import java.util.List;
+
 public class BinarySearch {
 
+    public final Dictionary dictionaries;
+
+    public BinarySearch() {
+        this.dictionaries = new Dictionary();
+    }
+
+    public static Word binarySearching(List<Word> words, String word_target) {
+        int low = 0;
+        int high = words.size()-1;
+        int mid;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+            if (words.get(mid).getWordTarget().compareTo(word_target) < 0) {
+                low = mid + 1;
+            } else if (words.get(mid).getWordTarget().compareTo(word_target) > 0) {
+                high = mid - 1;
+            } else {
+                return words.get(mid);
+            }
+        }
+
+        return null;
+    }
 }
