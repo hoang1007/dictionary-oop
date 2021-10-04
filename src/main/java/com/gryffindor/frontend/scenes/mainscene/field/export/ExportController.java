d84fe6389df3c5cd1b2398dbd5043ae5356d2d95
HoangAnh-it <vha31102002@gmail.com>
2021-10-04 17:12:58 +0700

    Thêm tiêu đề bà định dạng file

--- src/main/java/com/gryffindor/frontend/scenes/mainscene/field/export/ExportController.java
+++ src/main/java/com/gryffindor/frontend/scenes/mainscene/field/export/ExportController.java
@@ -2,6 +2,27 @@ package com.gryffindor.frontend.scenes.mainscene.field.export;
 
 import com.gryffindor.frontend.scenes.mainscene.field.IController;
 
+import java.io.File;
+
+import javafx.stage.FileChooser.ExtensionFilter;
+
 public class ExportController implements IController {
-  
+    // Khởi tạo
+    public static void initialize() {
+        String dic = "C:\\";
+        IController.fileChooser.setInitialDirectory(new File(dic));
+    }
+
+    public static void setTitleAndFileName(String title, String file_name) {
+        IController.fileChooser.setInitialFileName(file_name);
+        IController.fileChooser.setTitle(title);
+    }
+
+    public static void addExtensionFilter() {
+        // Định dạng file save
+        ExtensionFilter txt = new ExtensionFilter("TEXT files", "*.txt");
+        ExtensionFilter pdf = new ExtensionFilter("PDF", "*.pdf");
+        ExtensionFilter html = new ExtensionFilter("Web Page", "*.html");
+        IController.fileChooser.getExtensionFilters().addAll(txt, pdf, html);
+    }
 }
